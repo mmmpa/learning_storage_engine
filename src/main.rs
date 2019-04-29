0f2a23c8ca03124817e31e30a0d6df4bbce50acf
mmmpa <mmmpa.mmmpa@gmail.com>
2019-04-29 13:25:10 +0900

    updated

---
+++ src/main.rs
@@ -0,0 +1,2 @@
+mod most_simple;
+
---
+++ src/most_simple.rs
@@ -0,0 +1,64 @@
+use std::fs;
+use std::fs::OpenOptions;
+use std::error::Error;
+use std::io::Write;
+use std::str::FromStr;
+
+struct MostSimple {
+    path: String,
+}
+
+impl MostSimple {
+    fn new(path: &str) -> MostSimple {
+        let _ = fs::remove_file(path);
+
+        MostSimple {
+            path: path.to_string(),
+        }
+    }
+
+    fn set(&self, id: u32, data: &str) -> Result<(), String> {
+        let mut file = match OpenOptions::new().create(true).append(true).open(&self.path) {
+            Err(_) => return Err("open error".to_string()),
+            Ok(f) => f,
+        };
+        match write!(file, "{}::{}\n", id, data) {
+            Err(_) => return Err("write error".to_string()),
+            Ok(f) => f,
+        };
+
+        Ok(())
+    }
+
+    fn get(&self, id: u32) -> Option<String> {
+        let whole = match fs::read_to_string(&self.path) {
+            Err(_) => return None,
+            Ok(f) => f,
+        };
+
+        let mut matched = None;
+        for line in whole.lines() {
+            let s: Vec<&str> = line.split("::").collect();
+            let id_raw = s.get(0).unwrap();
+            let id_now = u32::from_str(id_raw).unwrap();
+            let data = s.get(1).unwrap();
+            if id == id_now {
+                matched = Some(data.to_string());
+            }
+        }
+
+        matched
+    }
+}
+
+#[test]
+fn test_get_set() {
+    let ms = MostSimple::new("./tmp/most_simple.txt");
+
+    ms.set(1, "data 1");
+    ms.set(2, "data 2-1");
+    ms.set(2, "data 2-2");
+    assert_eq!(ms.get(1).unwrap(), "data 1".to_string());
+    assert_eq!(ms.get(2).unwrap(), "data 2-2".to_string());
+    assert_eq!(ms.get(3), None);
+}
